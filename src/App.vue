111b8a00ac82a642d2088c1bba45479160cdbd9d
gammaSpeck <madhukm3141@gmail.com>
2024-06-12 01:14:09 +0530

    Added fab

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <v-main class="main-container">
+      <v-fab color="primary" icon="mdi-download" class="download-fab"></v-fab>
       <ResumeContainer />
     </v-main>
   </v-app>
@@ -13,5 +14,11 @@ import ResumeContainer from "./components/ResumeContainer.vue";
 <style scoped lang="scss">
 .main-container {
   background-color: lightgrey;
+}
+.download-fab {
+  position: fixed;
+  top: 2rem; /* Adjust this value to control the distance from the top */
+  right: 4rem; /* Adjust this value to control the distance from the right */
+  z-index: 1000; /* Ensures it stays above other elements */
 }
 </style>
